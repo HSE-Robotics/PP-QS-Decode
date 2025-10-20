516522f8a1b897f7846da0728a13b66a9ab8d99a
ericorozco <eorozco@harmonytx.org>
2025-10-20 13:59:41 -0500

    - Reggie Class added - TeleOp v1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Reggie.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Reggie {
+
+    // Declare hardware components as public to be accessible in OpModes
+    public DcMotor intakeMotor = null;
+    public DcMotor leftShooterMotor = null;
+    public DcMotor rightShooterMotor = null;
+    public Servo sorterServo = null;
+    public CRServo leftIndexerServo = null;
+    public CRServo rightIndexerServo = null;
+
+    // HardwareMap object
+    private HardwareMap hwMap = null;
+
+    /* Constructor */
+    public Reggie() {
+    }
+
+    /* Initialize hardware */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+
+        intakeMotor = hwMap.get(DcMotor.class, "int");
+        leftShooterMotor = hwMap.get(DcMotor.class, "LS");
+        rightShooterMotor = hwMap.get(DcMotor.class, "RS");
+
+        // Define and Initialize Servos
+        sorterServo = hwMap.get(Servo.class, "indServo");
+        leftIndexerServo = hwMap.get(CRServo.class, "LServo");
+        rightIndexerServo = hwMap.get(CRServo.class, "RServo");
+
+
+        // Set all motors to zero power
+        intakeMotor.setPower(0);
+        leftShooterMotor.setPower(0);
+        rightShooterMotor.setPower(0);
+
+        // Set all motors to run without encoders
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftShooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightShooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Set servo initial position
+        sorterServo.setPosition(0.25); // Example initial position
+    }
+
+
+    public void setShooterPower(double power, DcMotor shooter) {
+        shooter.setPower(power);
+    }
+    public void setIntakePower(double power, DcMotor Intake) {
+        Intake.setPower(power);
+    }
+    public void setSorterServoPosition(double position) {
+        sorterServo.setPosition(position);
+    }
+    public void indexerPower(double power, CRServo indexer) {indexer.setPower(power);}
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOp/ReggieTeleop_v1.java
@@ -0,0 +1,181 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.TeleOp;
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.function.Supplier;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.pedroPathing.Reggie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ *
+ * @author Gerry DLIII - 18908 Mighty Hawks
+ * @version 1.0, 02/11/2024
+ */
+@TeleOp(name = "Reggie_TeleOP", group = "LM1 Reggie")
+public class ReggieTeleop_v1 extends OpMode {
+    //private static final Logger log = LoggerFactory.getLogger(AyCrush2P_PP.class);
+    //Pedro Pathing Variables
+    private Follower follower;
+    private Reggie Miller;
+    public static Pose startingPose;
+    //Crush Variables
+    private enum IntakeState{
+        IN,
+        OUT
+    }
+    private enum ScoringSelection{
+        SAMPLE,
+        SPECIMEN
+    }
+    ScoringSelection scoringSelection = ScoringSelection.SPECIMEN;
+    public enum ScoringState{
+        IDLE,
+        ACCELERATING,
+        SHOOTING,
+        COOLING_DOWN
+    }
+    ScoringState artifactScoringState = ScoringState.IDLE;
+    private enum IntakeCurrState{
+        IN,
+        OUT
+    }
+
+    int shootingState = 0;
+    private boolean IntakeElbowDown = false;
+    private boolean OuttakeElbowDown = false;
+    private boolean OuttakeClawOpen = false;
+    private boolean IntakeClawOpen = true;
+    private boolean IntakeWristChanged = false;
+    private boolean IntakeSliderChanged = false;
+    public boolean inEndgame;
+    public boolean blockpos;
+    public ElapsedTime shootingTime, playTime;
+
+    public boolean hangersMoving= false;
+
+    /**
+     * This initializes the drive motors as well as the Follower and motion Vectors.
+     */
+    @Override
+    public void init() {
+        //Pedro Pathing
+//        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
+        follower.update();
+        Miller = new Reggie();
+        Miller.init(hardwareMap);
+
+        shootingTime = new ElapsedTime();
+        playTime = new ElapsedTime();
+        inEndgame = false;
+
+
+       // playTime.reset();
+    }
+    /** This method is called once at the start of the OpMode. **/
+    @Override
+    public void start() {
+        follower.startTeleopDrive();
+        playTime.reset();
+    }
+    /**
+     * This runs the OpMode. This is only drive control with Pedro Pathing live centripetal force
+     * correction.
+     */
+    @Override
+    public void loop() {
+        /**Pedro Pathing Driving
+         *
+         */
+        follower.update();
+        follower.setTeleOpDrive(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x, true);
+
+        switch (artifactScoringState){
+            case IDLE:
+                        if(gamepad1.left_bumper && shootingState==0){
+                            shootingState=1;
+                            Miller.setShooterPower(100,Miller.leftShooterMotor);
+                            setScoringState(ScoringState.ACCELERATING);
+
+
+                        } else if (!gamepad1.cross) {
+                            shootingState = 0;
+                        }
+                        break;
+            case ACCELERATING:
+                        if(gamepad1.left_bumper && shootingState==0 && shootingTime.seconds()>3.0){
+                            shootingState=1;
+                            Miller.indexerPower(1.0,Miller.leftIndexerServo);
+                            setScoringState(ScoringState.SHOOTING);
+                        } else if (!gamepad1.cross) {
+                            shootingState = 0;
+                        }
+                        break;
+            case SHOOTING:
+                        if(shootingTime.seconds()>2.0){
+                            Miller.indexerPower(0.0,Miller.leftIndexerServo);
+                            Miller.setShooterPower(0.0,Miller.leftShooterMotor);
+                            shootingState=0;
+                            setScoringState(ScoringState.IDLE);
+                        }
+                break;
+
+        }
+        if(gamepad1.left_trigger>0.125){
+            Miller.setIntakePower(gamepad1.left_trigger, Miller.intakeMotor);
+        } else if (gamepad1.right_trigger>0.125) {
+            Miller.setIntakePower(gamepad1.right_trigger, Miller.intakeMotor);
+        }else{
+            Miller.setIntakePower(0.0, Miller.intakeMotor);
+        }
+
+
+        //General code for both options (1 or 2 Players)
+        if(playTime.seconds()>110 && !hangersMoving){
+            hangersMoving = true;
+        }if(playTime.seconds()>40 && !inEndgame){
+            gamepad1.rumble(3000);
+            gamepad2.rumble(3000);
+            endgameLED();
+            inEndgame = true;
+        }
+
+        telemetry.addData("Play Time: ", playTime.seconds() );
+
+
+
+
+    }
+
+    public void endgameLED(){
+        gamepad1.setLedColor(255,0,0,500);
+        gamepad2.setLedColor(0,0,255,500);
+    }
+    public void setScoringState(ScoringState pState) {
+        artifactScoringState = pState;
+        shootingTime.reset();
+
+    }
+
+}
